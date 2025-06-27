0f5224927800d30750792849acdeeb774ce62ca7
nishithat-28 <nish.tirumalaraju@gmail.com>
2025-06-27 16:48:59 +0530

    Add Sidebar

--- chatbot_streamlit.py
+++ chatbot_streamlit.py
@@ -1,58 +1,26 @@
 import streamlit as st
 import google.generativeai as genai
 from google.api_core.exceptions import InvalidArgument
-
+import time
 
 st.markdown(
     """
     <style>
-    .stApp {
-        background-image: url('https://img.freepik.com/free-vector/digital-dark-wavy-background_23-2148388254.jpg');
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-    }
-
-    .stButton > button {
-        background-color: #DD0000;
-        color: white;
-        border: none;
-        padding: 10px 20px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        cursor: pointer;
-        border-radius: 12px;
-    }
-
-    .stTextInput > div > div > input {
-        background-color: #FFFFFF; 
-        color: #00154D;
-    }
-
     h1 {
-        color: white;
         text-align: center;
         font-family : Verdana, sans-serif;
         font-weight: 100;
     }
     h2 {
-        color: white;
         text-align: center;
         font-family : Verdana, sans-serif;
         font-weight: 100;
     }
     h3 {
-        color: white;
         text-align: center;
         font-family : Verdana, sans-serif;
         font-weight: 100
     }
-
-    
-    
     </style>
     """,
     unsafe_allow_html=True
@@ -68,28 +36,35 @@ with st.sidebar:
         st.session_state.api_key = ""
     st.subheader("API Key")
     st.session_state.api_key = st.text_input("Enter your Gemini API Key", type="password", key="api_key_input")
-    
 
+    
 if not st.session_state.api_key:
     st.warning("Please enter your Gemini API key in the sidebar.")
 
+def stream_data(text):
+    for word in text.split(" "):
+        yield word + " "
+        time.sleep(0.05)
 try:
     genai.configure(api_key=st.session_state.api_key)
     model = genai.GenerativeModel(model_name='models/gemini-2.0-flash-lite') # gemini-2.0-flash-lite, gemini-1.5-pro
     if 'chat' not in st.session_state:
         st.session_state.chat = model.start_chat(history=[])
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
     
-    qn = st.chat_input("Type Something:", key="home_input")
+    for message in st.session_state.messages:
+        st.chat_message(message["role"]).write(message["content"])
+    
+    qn = st.chat_input("Type Something ...", key="home_input")
     if qn:
         st.chat_message("user").write(qn)
+        st.session_state.messages.append({"role":"user", "content":qn})
         with st.spinner("Generating response..."):
             response = st.session_state.chat.send_message(qn)
-        st.chat_message("ai").write(response.text)
+        st.chat_message("ai").write_stream(stream_data(response.text))
+        st.session_state.messages.append({"role":"ai", "content":response.text})
         
 
 except Exception as e:
     st.error(f"❌ Failed to initialize Gemini model: {e}")
-
-
-
-    
